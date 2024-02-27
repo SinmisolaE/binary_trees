6c85b7e43c40be69b08f96bc319818247dff2ca6
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-27 09:18:59 +0000

    3-binary_tree_delete.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: parent of new node
+ * @value: value of new node
+ * Return: pointer of new node or NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	if (parent->left)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+	return (new);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees_h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child
+ * @parent: the parent of new node
+ * @value: value of new node
+ * Return: pointer of new node, or NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (new);
+	new->value = value;
+	new->parent = parent;
+	if (parent->right)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+	return(new);
+}
