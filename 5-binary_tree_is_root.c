cf916e3ba8404a95a8a3c632da94cdd0544e6df0
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-28 00:41:14 -0800

    18-bianry_tree_uncle.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,12 +5,11 @@
  * @node: the node
  * Return: 1 if True, else 0
  */
-int binary_tree_is_root(binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
 	if (!(node->parent))
 		return (1);
-	else
-		return (0);
+	return (0);
 }
