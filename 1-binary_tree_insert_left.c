eac926186cf21731bfe7a27cbf5527fe568f9513
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-28 00:18:54 -0800

    18-bianry_tree_uncle.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,7 +6,7 @@
  * @value: value of new node
  * Return: pointer of new node or NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
