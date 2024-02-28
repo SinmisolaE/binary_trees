30f16a903e589fc0bcea64b7c8a1dd088cf7fcd3
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-28 04:29:50 -0800

    Debugging binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,9 +5,9 @@
  * @tree: pointer to root node
  * Return: the height else 0
  */
-size_t binary_tree_height(binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t x, y = 0;
+	size_t x, y;
 
 	if (!tree)
 		return (0);
