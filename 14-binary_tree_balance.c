6212df71299c65b0de60084d61d1210479a32ae6
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-28 05:28:58 -0800

    Debugging binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,8 +7,8 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t x;
-	size_t y;
+	int x;
+	int y;
 
 	if (!tree)
 		return (0);
@@ -16,3 +16,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 	y = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 	return (x - y);
 }
+/*
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t
+	if (!tree)
+		return (0);
+	x = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	y = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (x > y ? x : y);
+}*/
