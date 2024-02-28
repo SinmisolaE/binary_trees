930a7ebb533c353fddc38e6a03e2a6377d4718b9
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-28 00:25:13 -0800

    18-bianry_tree_uncle.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,11 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	new = malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
-	new->n = value;
-	new->parent = parent;
 	if (parent->left)
 	{
 		new->left = parent->left;
