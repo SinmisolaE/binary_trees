6be600d75a2f37f1f876d6cff0edf37aa2dc2756
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-28 04:55:59 -0800

    Debugging binary trees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,14 +5,14 @@
  * @tree: pointer to the root node
  * Return: count else 0
  */
-binary_tree_nodes(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes;
 
 	nodes = 0;
 	if (!tree)
 		return (0);
-	nodes += (!(tree->left) || !(tree->right)) ? 1 : 0;
+	nodes = ((tree->left) || (tree->right)) ? 1 : 0;
 	nodes += binary_tree_nodes(tree->left);
 	nodes += binary_tree_nodes(tree->right);
 	return (nodes);
