a28936f5c38583245f66fd958f6c9b2233622af4
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-28 13:12:20 -0800

    Debugging binary trees

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -5,7 +5,7 @@
  * @node: pointer to the find to find the sibling
  * Return: pointer to the sibling node
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !(node->parent))
 		return (NULL);
