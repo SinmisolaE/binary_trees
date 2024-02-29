ce2dd63a72e7b9cc53ed6b0d4da04473ef76d50a
SinmisolaE <sinmisolaajao@gmail.com>
2024-02-29 04:53:30 -0800

    Debugging binary trees

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	size_t x;
 
 	if (!tree)
-		return (NULL);
+		return (0);
 	x = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 	return (x);
 }
